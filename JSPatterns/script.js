e5560f7b4350dffd04545784a2df1dc498ead494
Kutay Ozkoc <kutayozkoc99@gmail.com>
2020-08-31 14:33:04 +0300

    FactoryPattern Added

--- JSPatterns/script.js
+++ JSPatterns/script.js
@@ -101,3 +101,115 @@ var extended = (function(m){
 
 extended.sayHello();
 console.log(extended.getProducts());
+
+
+//Singleton Pattern
+
+/*
+    
+    Sometimes we just want to use the reference of the first object we produced we dont need produce new objects
+*/
+
+var singleton = (function(){
+    var instance; // it is private
+
+    var createInstance = function(){ // it is private
+        return{
+            randomnumber :Math.random()
+        }
+    }
+
+    return{ getInstance : function(){
+            if(!instance){
+                instance = new createInstance(); // if instance is empty fill it 
+            }
+            return instance // instance is not empty
+
+        }
+
+    }
+})();
+
+
+instance1 =  singleton.getInstance();
+instance2 =  singleton.getInstance(); // we create different time this variables but values are same because references are same
+
+console.log(instance1.randomnumber,instance2.randomnumber);
+
+
+var singleton2 =(function () {
+    var instance2;
+
+    function constProducts(){
+        const products = [
+            {name:"telephone"},
+            {name:"TV"}
+
+        ];
+
+
+        const add = function(params) {
+            products.push(params);
+        }
+
+        const getPro = function() {
+            return products;
+        }
+
+        return constProducts,add,getPro
+    }
+
+
+
+})();
+
+
+//Factory Pattern
+
+/*Function creates object 
+
+    this objects are so similar so we use this pattern
+    factory create our patterns
+*/
+
+function Factory(){
+
+    this.createEmployee = function(type){
+
+        var employee;
+
+        if(type==='fulltime'){
+            employee = new FullTime();    
+        }else if(type==='parttime'){
+            employee = new PartTime();
+        } 
+        employee.type = type;
+        employee.say = function(){
+            console.log(this.type + ' : saatlik ücreti : '+ this.hourly);
+        }
+        return employee;
+    }
+}
+
+var FullTime = function(){
+    this.hourly = '30TL';
+}
+
+var PartTime = function(){
+    this.hourly = '20TL';
+}
+
+
+
+var factory = new Factory();
+
+var employees = [];
+
+employees.push(factory.createEmployee('fulltime'));
+employees.push(factory.createEmployee('parttime'));
+employees.push(factory.createEmployee('parttime'));
+employees.push(factory.createEmployee('fulltime'));
+
+employees.forEach(function(emp){
+    emp.say();
+})
